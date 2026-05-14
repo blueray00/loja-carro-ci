5b4d36d3ee9ea8d5b7f3014fc0bac54a0c3e7d85
blueray00 <raynara.gustavo@ifrn.escolar.edu.br>
2026-05-14 20:34:46 -0300

    Corrige falha

--- src/test/java/br/org/edu/ifrn/LojaCarro/services/CarroServiceTest.java
+++ src/test/java/br/org/edu/ifrn/LojaCarro/services/CarroServiceTest.java
@@ -103,7 +103,7 @@ class CarroServiceTest {
 
         List<Carro> resultado = carroService.findAll();
 
-        assertEquals(999, resultado.size());
+        assertEquals(2, resultado.size());
         assertSame(carros, resultado);
         verify(carroRepository).findAll();
         verifyNoMoreInteractions(carroRepository);
